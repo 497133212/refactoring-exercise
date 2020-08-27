c0fe39cf3920939e88b9fda8e1fcbb32b8d45e70
Austin <497133212@qq.com>
2020-08-27 23:28:42 +0800

    [Refactoring-exercise][Austin] feature: implement create html output

--- src/statement.js
+++ src/statement.js
@@ -85,10 +85,30 @@ function printResult(data) {
     return result;
 }
 
+function generateHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += '<table>\n';
+    result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+    for(let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usdFormat(perf.amount)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>Amount owed is <em>${usdFormat(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCreadits}</em> credits</p>\n`;
+    return result;
+}
+
 function statement(invoice, plays) {
     return printResult(createStatementData(invoice, plays));
 }
 
+
+function createHtmlStatement(invoice, plays) {
+    return generateHtml(createStatementData(invoice, plays));
+}
+
 module.exports = {
-    statement
+    statement,
+    createHtmlStatement
 };
